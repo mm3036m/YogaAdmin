7563fc163477360b5e004543a3cdfd0e5b902198
mm3036m <mm3036m@gre.ac.uk>
2023-12-04 10:56:15 -0800

    api updated successfully

--- app/src/main/java/Helpers/ApiHelper.java
+++ app/src/main/java/Helpers/ApiHelper.java
@@ -4,9 +4,13 @@ import android.os.AsyncTask;
 import android.widget.ProgressBar;
 
 import java.io.BufferedOutputStream;
-        import java.io.OutputStream;
-        import java.net.HttpURLConnection;
-        import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class ApiHelper extends AsyncTask<String, Void, String> {
 
@@ -45,7 +49,7 @@ public class ApiHelper extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... params) {
-        String apiUrl = "https://stuiis.cms.gre.ac.uk/COMP1424CoreWS/comp1424cw";
+        String apiUrl = "https://stuiis.cms.gre.ac.uk/COMP1424CoreWS/comp1424cw/SubmitClasses";
         String jsonData = params[0];
 
         try {
@@ -56,30 +60,62 @@ public class ApiHelper extends AsyncTask<String, Void, String> {
             urlConnection.setRequestMethod("POST");
 
             // Set the content type and length
-            urlConnection.setRequestProperty("Content-Type", "application/json");
-            urlConnection.setRequestProperty("Content-Length", String.valueOf(jsonData.length()));
+            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
 
             // Enable input/output streams
             urlConnection.setDoInput(true);
             urlConnection.setDoOutput(true);
+            String encodedParams = encodeParams(jsonData);
+            byte[] postData = encodedParams.getBytes("UTF-8");
+            urlConnection.setRequestProperty("Content-Length", String.valueOf(postData.length));
 
             // Write the JSON data to the output stream
             try (OutputStream os = new BufferedOutputStream(urlConnection.getOutputStream())) {
-                os.write(jsonData.getBytes());
+                os.write(postData);
                 os.flush();
             }
 
             // Get the response from the server
             int responseCode = urlConnection.getResponseCode();
+
             if (responseCode == HttpURLConnection.HTTP_OK) {
 
-                return "Upload successful";
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    // Print or return the API response
+                    System.out.println("API Response: " + response.toString());
+                    return "Upload successful";
+                }
             } else {
-                return "Error: " + responseCode;
+
+
+                try (BufferedReader errorReader = new BufferedReader(new InputStreamReader(urlConnection.getErrorStream()))) {
+                    StringBuilder errorResponse = new StringBuilder();
+                    String line;
+                    while ((line = errorReader.readLine()) != null) {
+                        errorResponse.append(line);
+                    }
+                    // Print or return the error response
+                    System.out.println("Error Response: " + errorResponse.toString());
+                    return "Error: " + responseCode;
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
             return "Error: " + e.getMessage();
+        }
+    }
+    private String encodeParams(String jsonPayload) {
+        try {
+            return "jsonpayload=" + URLEncoder.encode(jsonPayload, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return "";
         }
     }
 }
